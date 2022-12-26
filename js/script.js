b838a1e3c42c55adf56ae90b0a15785449633bbe
moussafia <moussafia.mohammed1999@gmail.com>
2022-12-26 09:22:26 +0100

    brief partie SAS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// style bar time
+const bars=document.querySelectorAll(".round-time-bar");
+
+bars.forEach((bar)=>{
+    bar.classList.remove("round-time-bar");
+    bar.offsetWidth;
+    console.log(bar);
+    bar.classList.add("round-time-bar");
+ 
+  });
