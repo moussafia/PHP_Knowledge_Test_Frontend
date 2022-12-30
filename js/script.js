c8524b7ba3853d7a45d3165e4f6321b110b5bb6e
moussafia <moussafia.mohammed1999@gmail.com>
2022-12-30 21:17:05 +0100

    j'arrange les fichier

--- js/script.js
+++ js/script.js
@@ -1,110 +1,130 @@
 //==============show function=======================
-showQuizz();
+showQuizz(score);
 //====================== import data.js =========================
-import {quizz} from "./data.js";
+import { quizz } from "./data.js";
 
 //====================== get elementfrom =========================
 let score=0;
-
 //==================== display quizz in html file =======================
-function showQuizz(){
-  var randomQuestion= quizz.sort(()=>Math.random() - 0.5);
-  var arrAnswrCount=[0,1,2,3];
-  var arrAnswrRandom=arrAnswrCount.sort(()=>Math.random() - 0.5);
+function showQuizz(score) {
+  var randomQuestion = quizz.sort(() => Math.random() - 0.5);
+  var arrAnswrCount = [0, 1, 2, 3];
+  var arrAnswrRandom = arrAnswrCount.sort(() => Math.random() - 0.5);
 
-// ======================declaration variable vide================
-var boxQuestion=document.querySelector(".question strong");
-var answer1=document.querySelector(".answer1 strong");
-var answer2=document.querySelector(".answer2 strong");
-var answer3=document.querySelector(".answer3 strong");
-var answer4=document.querySelector(".answer4 strong");
+  // ======================declaration variable vide================
+  var boxQuestion = document.querySelector(".question strong");
+  var answer1 = document.querySelector(".answer1 strong");
+  var answer2 = document.querySelector(".answer2 strong");
+  var answer3 = document.querySelector(".answer3 strong");
+  var answer4 = document.querySelector(".answer4 strong");
 
-// ======================declaration button answer================
-var next=document.getElementById('btn-skip');
-var btnAnswer1=document.querySelector(".answer1");
-var btnAnswer2=document.querySelector(".answer2");
-var btnAnswer3=document.querySelector(".answer3");
-var btnAnswer4=document.querySelector(".answer4");
-var buttonAnswers=[btnAnswer1,btnAnswer2,btnAnswer3,btnAnswer4];
+  // ======================declaration button answer================
+  var next = document.getElementById("btn-skip");
+  var btnAnswer1 = document.querySelector(".answer1");
+  var btnAnswer2 = document.querySelector(".answer2");
+  var btnAnswer3 = document.querySelector(".answer3");
+  var btnAnswer4 = document.querySelector(".answer4");
+  var buttonAnswers = [btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4];
 
-const bar = document.querySelector(".round-time-bar");
+  const bar = document.querySelector(".round-time-bar");
 
   // ==========================initialisation des variables=================
-    boxQuestion.innerHTML='';
-    answer1.innerHTML='';
-    answer2.innerHTML='';
-    answer3.innerHTML='';
-    answer4.innerHTML='';
-    let index=0;
+  boxQuestion.innerHTML = "";
+  answer1.innerHTML = "";
+  answer2.innerHTML = "";
+  answer3.innerHTML = "";
+  answer4.innerHTML = "";
+  let index = 0;
   // =================== Appel function display==========================
-  next.style.display='none';
-  display(index);
+  next.style.display = "none";
+  display(index,next,score);
   index++;
-  next.addEventListener('click', ()=>{next.style.display='none';progressBarTiming(bar);
+  next.addEventListener("click", () => {
+    next.style.display = "none";
+    progressBarTiming(bar);
     removeClassesAndOpacity(buttonAnswers);
-        display(index);
-        if(index<=randomQuestion.length){  
-          index++;
-        } 
-        
-  }
-  ); 
-  
-  function display(index){
-    boxQuestion.innerHTML=randomQuestion[index].question;
-      answer1.innerHTML=randomQuestion[index].answers[arrAnswrRandom[0]];
-      answer2.innerHTML=randomQuestion[index].answers[arrAnswrRandom[1]];
-      answer3.innerHTML=randomQuestion[index].answers[arrAnswrRandom[2]];
-      answer4.innerHTML=randomQuestion[index].answers[arrAnswrRandom[3]];
-         
-    // ======================declaration variable rempli================
-        var asw1=document.querySelector(".answer1 strong").textContent;
-        var asw2=document.querySelector(".answer2 strong").textContent;
-        var asw3=document.querySelector(".answer3 strong").textContent;
-        var asw4=document.querySelector(".answer4 strong").textContent;
-        var asws=[asw1,asw2,asw3,asw4];
+    display(index,next,score);
+    if (index < randomQuestion.length) {
+      index++;
+    }
+  });
 
-        for(let j=0;j<buttonAnswers.length;j++){
-          buttonAnswers[j].addEventListener('click',()=>{
-                removeClassesAndOpacity(buttonAnswers);
-                    if( asws[j].localeCompare(randomQuestion[index].trueAnswer[0]) == 0){
-                        countScores(score);
-                        buttonAnswers[j].classList.add("correct-answer");
-                        for(let i=0;i<buttonAnswers.length;i++){
-                          buttonAnswers[i].style.pointerEvents='none';
-                          if(i!=j){
-                            buttonAnswers[i].style.opacity='0';
-                          }
-                        }
-                    }else{
-                      buttonAnswers[j].classList.add("incorrect-answer");
-                        for(let i=0;i<buttonAnswers.length;i++){
-                          buttonAnswers[i].style.pointerEvents='none';
-                          if(i!=j){
-                            if(randomQuestion[index].answers[arrAnswrRandom[i]].localeCompare(randomQuestion[index].trueAnswer[0]) == 0){
-                              buttonAnswers[i].classList.add("correct-answer");
-                            } else buttonAnswers[i].style.opacity='0';
-                          }
-                        }
-                    }
-                next.style.display='block';});
-     }
-}
-}
-function removeClassesAndOpacity(array){
-  for(let i=0;i<array.length;i++){
-    array[i].classList.remove("correct-answer", "incorrect-answer");
-    array[i].style.opacity='1';
-    array[i].style.pointerEvents='auto';
+  function display(index,next,score) {
+    document.querySelector(".lesNotes span").innerHTML = score;
+    boxQuestion.innerHTML = randomQuestion[index].question;
+    answer1.innerHTML = randomQuestion[index].answers[arrAnswrRandom[0]];
+    answer2.innerHTML = randomQuestion[index].answers[arrAnswrRandom[1]];
+    answer3.innerHTML = randomQuestion[index].answers[arrAnswrRandom[2]];
+    answer4.innerHTML = randomQuestion[index].answers[arrAnswrRandom[3]];
+
+    // ======================declaration variable rempli================
+    var asw1 = document.querySelector(".answer1 strong").textContent;
+    var asw2 = document.querySelector(".answer2 strong").textContent;
+    var asw3 = document.querySelector(".answer3 strong").textContent;
+    var asw4 = document.querySelector(".answer4 strong").textContent;
+    var asws = [asw1, asw2, asw3, asw4];
+
+    EventAnswerUser(0,buttonAnswers,asws,randomQuestion,index,next,score);
+    EventAnswerUser(1,buttonAnswers,asws,randomQuestion,index,next,score);
+    EventAnswerUser(2,buttonAnswers,asws,randomQuestion,index,next,score);
+    EventAnswerUser(3,buttonAnswers,asws,randomQuestion,index,next,score);
+   
+    
   }
-}  
-function progressBarTiming(array){
+}
+
+function answerUser(j,buttonAnswers,asws,randomQuestion,index,next,score) {
+
+  removeClassesAndOpacity(buttonAnswers);
+  if (asws[j].localeCompare(randomQuestion[index].trueAnswer[0]) == 0) {
+    buttonAnswers[j].classList.add("correct-answer");
+    for (let i = 0; i < buttonAnswers.length; i++) {
+      buttonAnswers[i].style.pointerEvents = "none";
+      if (i != j) {
+        buttonAnswers[i].style.opacity = "0";
+      }
+    }
+    score++;
+
+  } else {
+    buttonAnswers[j].classList.add("incorrect-answer");
+    for (let i = 0; i < buttonAnswers.length; i++) {
+      buttonAnswers[i].style.pointerEvents = "none";
+      if (i != j) {
+        if (
+          randomQuestion[index].answers[arrAnswrRandom[i]].localeCompare(
+            randomQuestion[index].trueAnswer[0]
+          ) == 0
+        ) {
+          buttonAnswers[i].classList.add("correct-answer");
+        } else buttonAnswers[i].style.opacity = "0";
+      }
+    }
+  }
+  next.style.display = "block";
+}
+
+
+
+function EventAnswerUser(j,buttonAnswers,asws,randomQuestion,index,next) {
+  buttonAnswers[j].addEventListener("click", () => {
+    
+    answerUser(j,buttonAnswers,asws,randomQuestion,index,next);
+  });
+}
+
+
+function removeClassesAndOpacity(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.remove("correct-answer", "incorrect-answer");
+    array[i].style.opacity = "1";
+    array[i].style.pointerEvents = "auto";
+  }
+}
+
+
+function progressBarTiming(array) {
   array.classList.remove("round-time-bar");
   array.offsetWidth;
   array.classList.add("round-time-bar");
-}
-function countScores(variable){
-    
-    variable++;
-    return document.querySelector(".lesNotes span").innerHTML=variable;
 }
